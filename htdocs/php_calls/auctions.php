5240471c554054cda8fad818623f23e4dd0434f4
seabornloyalis <chad.myles@tufts.edu>
2016-02-12 17:01:04 +0000

    auctions.php echoes the result of common calls for auction data Currently untested

---
+++ htdocs/php_calls/auctions.php
@@ -0,0 +1,64 @@
+<?php
+
+    require 'db_functions.php';
+
+function get_users_auctions($userID) {
+	$query = "SELECT asking_price, end_time, title, COUNT(*) as count
+				FROM (auction JOIN book
+				ON auction.ISBN = book.ISBN) JOIN view
+				ON auction.auctionID = view.auctionID
+				WHERE auction.sellerID = " . $userID ." ;";
+	$result = run_query($query);
+	for (int $i = 0; $i < count($result); $i++) {
+		for (int $j = 0; $j < count($result[$i]); $j++) {
+			echo $result[$i][$j];
+		}
+	}
+}
+
+function get_all_active_auctions() {
+	$current_time = date('Y-m-d H:i:s', time());
+	$query = "SELECT asking_price, end_time, title, COUNT(*) as count
+				FROM (auction JOIN book
+				ON auction.ISBN = book.ISBN) JOIN view
+				ON auction.auctionID = view.auctionID
+				WHERE auction.end_time > " . $current_time . " ;";
+	$result = run_query($query);
+	$result = run_query($query);
+	for (int $i = 0; $i < count($result); $i++) {
+		for (int $j = 0; $j < count($result[$i]); $j++) {
+			echo $result[$i][$j];
+		}
+	}
+}
+
+function get_all_auctions() {
+	$query = "SELECT asking_price, end_time, title, COUNT(*) as count
+				FROM (auction JOIN book
+				ON auction.ISBN = book.ISBN) JOIN view
+				ON auction.auctionID = view.auctionID;";
+	$result = run_query($query);
+	$result = run_query($query);
+	for (int $i = 0; $i < count($result); $i++) {
+		for (int $j = 0; $j < count($result[$i]); $j++) {
+			echo $result[$i][$j];
+		}
+	}
+}
+
+function get_auction($aucID) {
+	$query = "SELECT asking_price, end_time, title, COUNT(*) as count
+				FROM (auction JOIN book
+				ON auction.ISBN = book.ISBN) JOIN view
+				ON auction.auctionID = view.auctionID;
+				WHERE auction.userID = " . $aucID . " ;";
+	$result = run_query($query);
+	$result = run_query($query);
+	for (int $i = 0; $i < count($result); $i++) {
+		for (int $j = 0; $j < count($result[$i]); $j++) {
+			echo $result[$i][$j];
+		}
+	}
+}
+
+?>
